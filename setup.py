c4af076a2ab19d38a15b9441abfee18ba9ef68b7
Chad Dotson <chad@cdotson.com>
2016-06-26 02:51:16 -0500

    Restore parsing of requirements.txt in setup.py.

--- setup.py
+++ setup.py
@@ -3,13 +3,19 @@ from os.path import dirname, join
 
 version = '0.1.0'
 
-
-def read(fname):
-    return open(join(dirname(__file__), fname)).read()
-
+temp_install_reqs = []
+install_reqs = []
+dependency_links = []
 
 with open("requirements.txt", "r") as f:
-    install_reqs = f.readlines()
+    temp_install_reqs = list(map(str.strip, f.readlines()))
+
+for req in temp_install_reqs:
+    if req.startswith("https://"):
+        dependency_links.append(req)
+        install_reqs.append(req[req.find("egg=") + 4:].replace("-", "==", 1))
+    else:
+        install_reqs.append(req)
 
 setup(
     name='simple_email_chat_bot',
@@ -21,7 +27,7 @@ setup(
     author_email='chad@cdotson.com',
     description='',
     install_requires=install_reqs,
-    dependency_links=["https://github.com/chaddotson/pytools.git#egg=pytools"],
+    dependency_links=dependency_links,
     entry_points={
         'console_scripts': [
             'simple_email_chat_bot = bin.chatbot:main',
